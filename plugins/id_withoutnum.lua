637eab227bc5e152100bd2c7276e2f33fac8ed49
siktirmirza <alicowman79@gmail.com>
2016-08-12 16:24:22 +0430

    Create id_withoutnum.lua

---
+++ plugins/id_withoutnum.lua
@@ -0,0 +1,17 @@
+local function run(msg ,matches)
+if matches[1] == 'id' or 'ایدی' or 'آیدی' or 'آی دی' or 'ای دی' then
+return "🚩 نام شما :" ..string.gsub(msg.from.print_name, "_", " ").. "\n🚩 یوزر نیم شما : @"..(msg.from.username or '----').."\n🚩 ایدی شما : "..msg.from.id.."\n🚩 نام گروه :" ..string.gsub(msg.to.print_name, "_", " ").. "\n🚩 ایدی گروه : "..msg.to.id
+end
+end
+return = {
+ patterns = {
+  "^[!#/]([Ii][Dd])$",
+  "^([Ii][Dd]$",
+  "^ایدی$",
+  "^آیدی$",
+  "^آی دی$",
+  "^ای دی$"
+  },
+ run = run
+ }
+ --#SikTirMirza
