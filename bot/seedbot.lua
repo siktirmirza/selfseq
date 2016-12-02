a3fcaee7656da150d6e89b711a1db50065f793f0
siktirmirza <alicowman79@gmail.com>
2016-12-02 17:15:05 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -214,57 +214,24 @@ function create_config( )
     enabled_plugins = {
 	"admin",
     "onservice",
-    "inrealm",
-    "ingroup",
-    "inpm",
     "banhammer",
     "stats",
-    "anti_spam",
-    "owners",
-    "arabic_lock",
     "set",
     "get",
     "broadcast",
     "invite",
     "all",
-    "leave_ban",
-	"super",
 	"whitelist",
 	"msg_checks",
 	"send",
 	"prosend",
 	"save",
 	"plugins",
-	"addplug",
-	"boobs_fa"
+	"addplug"
     },
     sudo_users = {205906514},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
-An advanced administration bot based on TG-CLI written in Lua
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
-
-Our website 
-http://teleseed.seedteam.org/
-]],
+    about_text = [[:|]],
     help_text_realm = [[
 Realm Commands:
 
